55ae18580f018b6b1aca0301b3978e20b955b033
Cristian78 <kamadacristian@gmail.com>
2017-06-18 19:16:49 -0300

    calculo encuentro y correcciones de funciones

--- misFunciones.js
+++ misFunciones.js
@@ -8,6 +8,10 @@ function mostrar_ob2(accion) {
     if (accion=="encuentro" ){
         console.log("mostrar");
         document.getElementById('objeto2').style.display = 'block';
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'block';
+        document.getElementById('3').style.display = 'none';
+        document.getElementById('ace').style.display = 'none';
     }
 
     else if (accion=="mru"){
@@ -33,11 +37,13 @@ function reset() {
     document.getElementById("a").value="";
     document.getElementById("Xo2").value="";
     document.getElementById("Vo2").value="";
-    document.getElementById("t2").value="";
-    document.getElementById("a2").value="";
     document.getElementById("Vfr").value="";
     document.getElementById("Xr").value="";
-    document.getElementById("Tfr").value="";
+    document.getElementById("Af").value="";
+    document.getElementById("Te").value="";
+    document.getElementById("D1").value="";
+    document.getElementById("D2").value="";
+
 }
 
 function convert_unidades(id) {
@@ -46,7 +52,7 @@ function convert_unidades(id) {
         return (1*1000);
     }
     if(id=="Centimetros") {
-        return (1*100);
+        return (1/100);
     }
     if(id=="km/h") {
         return (1000/3600);
@@ -67,15 +73,17 @@ function verifvaloringresado(id) {
 }
 
 function calculo() {
-    var x=document.getElementById("mru");
-    var y=document.getElementById("mruv");
-    var z=document.getElementById("encuentro");
+
+
     var Xo=Number(document.getElementById("Xo").value) * convert_unidades("unidadXo");
     var Vo=Number(document.getElementById("Vo").value) * convert_unidades("unidadVo");
     var t=Number(document.getElementById("t").value);
     var a=Number(document.getElementById("a").value);
+    var d=Number(document.getElementById("Xo2").value) * convert_unidades("unidadXo2");
+    var Vo2=Number(document.getElementById("Vo2").value) * convert_unidades("unidadVo2");
 
-    if(x){
+
+    if(document.getElementById("mru").checked===true){
         var Xf=Xo+Vo*t;
         var Vf=(Xf-Xo)/t;
         document.getElementById("Vfr").innerHTML="Velocidad="+Vf+"m/s";
@@ -83,7 +91,7 @@ function calculo() {
     }
 
 
-    if(y){
+    else if(document.getElementById("mruv").checked===true){
         var Xfv=Xo+Vo*t+1/2*a*(t*t);
         var Vfv=Vo+a*t;
         var av=(Vfv-Vo)/(t);
@@ -92,5 +100,12 @@ function calculo() {
         document.getElementById("Af").innerHTML="Aceleracion="+av+"m/s";
     }
 
-    
+    else if(document.getElementById("encuentro").checked===true){
+        var te=d/(Vo+Vo2);
+        var d1=Vo*te;
+        var d2=Vo2*te;
+        document.getElementById("Te").innerHTML="Tiempo de encuentro="+te+"s";
+        document.getElementById("D1").innerHTML="distancia recorrida objeto 1="+d1+"m";
+        document.getElementById("D2").innerHTML="distancia recorrida objeto 2="+d2+"m";
+    }
 }
