9671dae6b68577996a8ce5d25c71282f9e301178
Cristian78 <kamadacristian@gmail.com>
2017-06-19 00:13:17 -0300

    graficos

--- misFunciones.js
+++ misFunciones.js
@@ -5,7 +5,7 @@
 function mostrar_ob2(accion) {
     console.log("accion");
 
-    if (accion=="encuentro" ){
+    if (accion==="encuentro" ){
         console.log("mostrar");
         document.getElementById('objeto2').style.display = 'block';
         document.getElementById('1').style.display = 'none';
@@ -14,12 +14,12 @@ function mostrar_ob2(accion) {
         document.getElementById('ace').style.display = 'none';
     }
 
-    else if (accion=="mru"){
+    else if (accion==="mru"){
         console.log("ocultar");
         document.getElementById('objeto2').style.display = 'none';
         document.getElementById('ace').style.display = 'none';
     }
-    if (accion=="mruv"){
+    if (accion==="mruv"){
         console.log("ocultar");
         document.getElementById('objeto2').style.display = 'none';
         document.getElementById('ace').style.display = 'block';
@@ -48,13 +48,13 @@ function reset() {
 
 function convert_unidades(id) {
     id=document.getElementById(id).value;
-    if(id=="Kilometros") {
-        return (1*1000);
+    if(id==="Kilometros") {
+        return (1000);
     }
-    if(id=="Centimetros") {
+    if(id==="Centimetros") {
         return (1/100);
     }
-    if(id=="km/h") {
+    if(id==="km/h") {
         return (1000/3600);
     }
     return 1;
@@ -86,6 +86,7 @@ function calculo() {
     if(document.getElementById("mru").checked===true){
         var Xf=Xo+Vo*t;
         var Vf=(Xf-Xo)/t;
+        dib1();
         document.getElementById("Vfr").innerHTML="Velocidad="+Vf+"m/s";
         document.getElementById("Xr").innerHTML="Distancia="+Xf+"metros";
     }
@@ -95,6 +96,7 @@ function calculo() {
         var Xfv=Xo+Vo*t+1/2*a*(t*t);
         var Vfv=Vo+a*t;
         var av=(Vfv-Vo)/(t);
+        dib2();
         document.getElementById("Vfr").innerHTML="Velocidad="+Vfv+"m/s";
         document.getElementById("Xr").innerHTML="Distancia="+Xfv+"metros";
         document.getElementById("Af").innerHTML="Aceleracion="+av+"m/s";
@@ -104,8 +106,142 @@ function calculo() {
         var te=d/(Vo+Vo2);
         var d1=Vo*te;
         var d2=Vo2*te;
+        dib3(te,d1,d2);
         document.getElementById("Te").innerHTML="Tiempo de encuentro="+te+"s";
         document.getElementById("D1").innerHTML="distancia recorrida objeto 1="+d1+"m";
         document.getElementById("D2").innerHTML="distancia recorrida objeto 2="+d2+"m";
+
     }
+}
+
+function dib1() {
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    var x=50;
+
+
+    ctx.lineWidth= 2;
+    ctx.strokeStyle = "black";
+
+    ctx.beginPath();
+    ctx.moveTo(x,500);
+    ctx.lineTo(400,500);
+    ctx.stroke();
+
+    var ti=(document.getElementById("t").value);
+    ctx.font = '20px Arial';
+    ctx.textAlign='center';
+    ctx.fillText(Number(ti)+""+"seg",200,520);
+
+    ctx.lineWidth= 2;
+
+    ctx.beginPath();
+    ctx.moveTo(x,500);
+    ctx.lineTo(x,200);
+    ctx.stroke();
+    var xi=(document.getElementById("Xo").value);
+    ctx.font = '20px Arial';
+    ctx.fillText(Number(xi)+"m",30,325);
+
+    ctx.lineWidth=3;
+
+    ctx.beginPath();
+    ctx.moveTo(x,500);
+    ctx.lineTo(400,200);
+    ctx.stroke();
+}
+
+function dib2() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var x = 50;
+
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+
+    ctx.beginPath();
+    ctx.moveTo(x, 500);
+    ctx.lineTo(400, 500);
+    ctx.stroke();
+
+    var ti = (document.getElementById("t").value);
+    ctx.font = '20px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText(Number(ti)+""+"seg", 200, 520);
+
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(x, 500);
+    ctx.lineTo(x, 200);
+    ctx.stroke();
+    var xi = (document.getElementById("Xo").value);
+    ctx.font = '20px Arial';
+    ctx.fillText(Number(xi)+"m", 30, 325);
+
+    ctx.lineWidth = 3;
+
+    ctx.beginPath();
+    ctx.moveTo(x, 500);
+    ctx.lineTo(400, 200);
+    ctx.stroke();
+}
+
+function dib3(te,y,z) {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var x = 25;
+
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+
+    ctx.beginPath();
+    ctx.moveTo(x, 500);
+    ctx.lineTo(350, 500);
+    ctx.stroke();
+
+    ctx.font = '20px Arial';
+    ctx.fillText(Number(te)+""+"seg", 350, 520);
+
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(x, 500);
+    ctx.lineTo(x, 200);
+    ctx.stroke();
+    ctx.font = '20px Arial';
+    ctx.fillText(Number(y)+"m", 10, 325);
+
+    ctx.lineWidth = 3;
+
+    ctx.beginPath();
+    ctx.moveTo(x, 500);
+    ctx.lineTo(350, 200);
+    ctx.stroke();
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "red";
+
+    ctx.beginPath();
+    ctx.moveTo(700, 500);
+    ctx.lineTo(350, 500);
+    ctx.stroke();
+
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(700, 500);
+    ctx.lineTo(700, 200);
+    ctx.stroke();
+    ctx.font = '20px Arial';
+    ctx.fillText(Number(z)+"m", 710, 325);
+
+    ctx.lineWidth = 3;
+
+    ctx.beginPath();
+    ctx.moveTo(700, 500);
+    ctx.lineTo(350, 200);
+    ctx.stroke();
 }
